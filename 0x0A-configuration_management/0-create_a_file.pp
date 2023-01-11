bb1f9335257fcdb49f5b3890065d928fed5572c6
Ephraim <ephraimigbinosa@gmail.com>
2023-01-11 14:19:29 +0100

    fix pp code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file {'tmp/school':
-    ensure => present,
-    mode => '0744',
-    owner => 'www-data'
-    group => 'www-data'
-    content => 'I love Puppet'
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
