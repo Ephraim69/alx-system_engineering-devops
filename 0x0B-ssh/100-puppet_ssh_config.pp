69a40e7a7a80d94fc3e1169d8b15846ea4d1b509
Ephraim <ephraimigbinosa@gmail.com>
2023-01-12 14:17:27 +0100

    bug fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Configure remote ssh_config file using puppet
 file_line {'Turnoff Password Authentication':
-  ensure => present,
-  path   => '~/.ssh/ssh_config',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line {'Declare Identity File':
-  ensure => present,
-  path   => '~/.ssh/ssh_config',
-  line   => '   IdentityFile ~/.ssh/school',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
